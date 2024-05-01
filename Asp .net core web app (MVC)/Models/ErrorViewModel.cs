4c0ca2ffc83a9dba4715a5e2cadee40a6335876b
cs-abhit <abhit.hinsu@confidosoft.in>
2024-05-01 12:35:17 +0530

    Default Home controller

---
+++ Asp .net core web app (MVC)/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Asp_.net_core_web_app__MVC_.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
